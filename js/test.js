f9c37df7f03f801481baf91eccbaabeac0b5fdcd
Mostafa Rokooie <mostafa.rokooie@gmail.com>
2013-05-17 03:54:25 +0430

    Implement test functionality of test page

--- js/test.js
+++ js/test.js
@@ -1,5 +1,18 @@
 
 var test = {
+    encrypt: function () {
+        enc = new Encryption(mykey.public, mykey.private);
+
+        ge('-test-encrypted').value = enc.encrypt(ge('-test-plain').value);
+    },
+
+    decrypt: function () {
+        enc = new Encryption(mykey.public, mykey.private);
+
+        enc.decrypt(ge('-test-encrypted').value, function (plain) {
+            ge('-test-plain').value = plain;
+        });
+    }
 };
 
 onLoadTasks.push(function () {
